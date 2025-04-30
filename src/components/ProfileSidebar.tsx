8e49cf3d10648b270540fff5971c57430c396031
Chhaya-Verma <chhayav300@gmail.com>
2025-04-30 23:59:32 +0530

    feat: create ProfileSidebar component

---
+++ src/components/ProfileSidebar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export const ProfileSidebar: React.FC = () => {
+    return (
+        <h1 className="text-2xl font-bold text-center text-[#6D2764]">Welcome is profile sidebar</h1>
+    )
+}
